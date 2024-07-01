ee235eaf15d46fe63bc9b53d706a614a2ad43f16
gonzalorportfolio <gromeroeducation@gmail.com>
2024-07-01 10:19:55 -0400

    update-parameters

--- 0-lecture-code/lecture.js
+++ 0-lecture-code/lecture.js
@@ -7,9 +7,9 @@ const friendsManager = {
   }
 }
 
-friendsManager.addFriend('ahmad');
-friendsManager.addFriend('brandon');
-friendsManager.addFriend('carmen');
+friendsManager.addFriend('Andy');
+friendsManager.addFriend('Cindy');
+friendsManager.addFriend('Patrick');
 // Here, friendsManager is the owner of addFriend. 
 // this.friends === friendsManager.friends
 
@@ -18,9 +18,9 @@ console.log(friendsManager.friends)
 
 
 // What about this is NOT consistent or predictable?
-friendsManager.addFriend('daniel');
+friendsManager.addFriend('Lauren');
 friendsManager.addFriend(true);
-friendsManager.friends.push('emmaneul');
+friendsManager.friends.push('Emmaneul');
 friendsManager.friends.push(42);
 // console.log(friendsManager.friends)
 
